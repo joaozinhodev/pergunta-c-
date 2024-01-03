e2db0b47c518b7e2567ce0eecb74b31253277fff
joaozinhodev <joaops40123@gmail.com>
2024-01-03 01:19:33 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("ai vai uma perguntinha");
+
+
+Console.WriteLine("quanto e a raiz quadrada de 4 ?");
+    
+var conta = 0;
+try
+{
+     conta = int.Parse(Console.ReadLine());
+}
+catch (FormatException)
+{
+    Console.WriteLine("Escreve direito o alignigena");
+}
+
+if (conta != 2)
+{
+    Console.WriteLine("ta errado maninho");
+}
+else if (conta == 2)
+{
+    Console.WriteLine("ta certo maninho");     
+}
+
